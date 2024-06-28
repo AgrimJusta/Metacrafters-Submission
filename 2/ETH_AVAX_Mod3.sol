a552c3da0dfb9dba1ef070c4fa1d7c8af2faef8e
Agrim <102186340+AgrimJusta@users.noreply.github.com>
2024-06-29 03:41:45 +0530

    Update ETH_AVAX_Mod3.sol

--- 2/ETH_AVAX_Mod3.sol
+++ 2/ETH_AVAX_Mod3.sol
@@ -1,52 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ETH_AVAX_Mod3 {
-    string public name = "Rupees";
-    string public symbol = "Rs";
-    uint8 public decimals = 18;
-    uint256 public totalSupply;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ETH_AVAX_Mod3 is ERC20 {
     address public owner;
 
-    mapping(address => uint256) public balanceOf;
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Mint(address indexed to, uint256 value);
-    event Burn(address indexed from, uint256 value);
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only owner can mint tokens");
-        _;
-    }
-
-    constructor() {
+    constructor() ERC20("Rupees", "Rs") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
         owner = msg.sender;
     }
 
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
     function mint(address to, uint256 amount) external onlyOwner {
-        require(to != address(0), "Mint to zero address");
-        totalSupply += amount;
-        balanceOf[to] += amount;
-        emit Mint(to, amount);
-        emit Transfer(address(0), to, amount);
+        _mint(to, amount);
     }
 
-    function transfer(address to, uint256 amount) external returns (bool) {
-        require(to != address(0), "Transfer to zero address");
-        require(balanceOf[msg.sender] >= amount, "Insufficient balance");
-        balanceOf[msg.sender] -= amount;
-        balanceOf[to] += amount;
-        emit Transfer(msg.sender, to, amount);
-        return true;
-    }
-
-    function burn(uint256 amount) external returns (bool) {
-        require(balanceOf[msg.sender] >= amount, "Insufficient balance to burn");
-        balanceOf[msg.sender] -= amount;
-        totalSupply -= amount;
-        emit Burn(msg.sender, amount);
-        emit Transfer(msg.sender, address(0), amount);
-        return true;
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
     }
 }
-
